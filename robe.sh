32507f02673dbccd5898070459edcc6be2cab46c
Geno Nullfree <nullfree.geno@gmail.com>
2020-05-20 20:39:30 -0400

    added more vairables

--- robe.sh
+++ robe.sh
@@ -1,5 +1,23 @@
+clear
+
 echo Hello, my name is Robe.
 echo It\'s pronounced ro-BEE.
 echo What is your name?
 read name
+echo What is your favorite color?
+read color
+echo What is your favorite food?
+read food
+echo How old are you?
+read age
+
+next_age=$[age+1]
+
+echo
 echo Hello $name
+echo On your next birthday, when you turn $next_age,
+echo I would like to paint your room $color,
+echo and make you your favorite $food.
+echo
+echo It was very nice to meet you $name!
+echo Goodbye for now!
